3d7bbb1d0f9c26c367d4ecdfb667d4f1a465564a
diego cuervo <dicuervo213@gmail.com>
2023-10-06 17:41:38 -0500

    modified

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -31,7 +31,4 @@ createPromise(2, 1500)
   console.log('❌ Rejected promise ${position} in ${delay}ms');
 });
 
-if (selectedDate <= currentDate) {
-  Notiflix.Notify.failure('');
-} 
 Notiflix.Notify.init({useFontAwesome: true,});
