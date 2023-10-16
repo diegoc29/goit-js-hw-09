5d96acef83d47fc9f7909363aa19bdd09e8338a5
diego cuervo <dicuervo213@gmail.com>
2023-10-15 20:07:49 -0500

    modified

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,34 +1,44 @@
 import Notiflix from "notiflix";
 
 function createPromise(position, delay) {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
-        resolve({position, delay});
+        resolve({ position, delay });
       } else {
-       reject({position, delay});
+        reject({ position, delay });
       }
     }, delay);
   });
 }
 
+document.getElementById("promise-form").addEventListener("submit", async function (event) {
+  event.preventDefault();
 
-function showSuccessNotification(position, delay) {
-  Notiflix.Notify.Success('✅ Fulfilled promise ${position} in ${delay}ms');
-}
+  const formData = new FormData(this);
+  const firstDelay = parseInt(formData.get("delay"));
+  const step = parseInt(formData.get("step"));
+  let amount = parseInt(formData.get("amount"));
 
-function showErrorNotification(position, delay) {
-  Notiflix.Notify.Failure('❌ Rejected promise ${position} in ${delay}ms');
+  const startButton = document.getElementById("start-button"); // Agregué esta línea para obtener el botón de inicio
 
-}
-
-createPromise(2, 1500)
-.then(({position, delay}) => {
-  console.log('✅ Fulfilled promise ${position} in ${delay}ms');
-})
-.catch(({position, delay}) =>{
-  console.log('❌ Rejected promise ${position} in ${delay}ms');
+  startButton.addEventListener("click", async function () {
+    if (amount <= 0) {
+      Notiflix.Notify.failure('Amount must be greater than 0');
+      return;
+    }
+  
+    for (let i = 0; i < amount; i++) {
+      const delay = firstDelay + i * step;
+      try {
+        const result = await createPromise(i + 1, delay);
+        Notiflix.Notify.Success(`✅ Fulfilled promise ${result.position} in ${result.delay}ms`);
+      } catch (error) {
+        Notiflix.Notify.failure(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
+      }
+    }
+  });
 });
 
-Notiflix.Notify.init({useFontAwesome: true,});
+Notiflix.Notify.init({ useFontAwesome: true });
